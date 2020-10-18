632863c7c4c0330b78292855ac36931dce842173
Jeendock <Jeendock@users.noreply.github.com>
2020-10-18 16:41:09 +0900

    2D

---
+++ Shooting/Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0f, 0.2f);
+
+        //if(transform.position.y >= 20)
+        //{
+        //    Destroy(this);
+        //}
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Shooting/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject BulletPrefap;
+    public int bulletDelay = 20;
+    private int bulletDelayCount = 0;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position = transform.position + new Vector3(-0.08f, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position = transform.position + new Vector3(0.08f, 0);
+
+
+        bulletDelayCount++;
+        if (bulletDelayCount >= bulletDelay)
+        {
+            var bulletObject = Instantiate(BulletPrefap);
+            bulletObject.transform.position = transform.position;
+            bulletDelayCount = 0;
+        }
+    }
+}
